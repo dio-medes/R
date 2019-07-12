b20d70ff13bfde3b3d9c61e66579cd5e924538c3
Oliver Richard Cutbill <olivercutbill@gmail.com>
2019-07-13 08:09:21 +1000

    Chapter 1

---
+++ Probability/Blitzstein Chapter 1.R
@@ -0,0 +1,30 @@
+# Chapter 1
+v <- c(3,1,4,1,5,9)
+length(v)
+
+#Sampling
+n <- 10; k <- 5
+sample(n,k,replace=TRUE) #Sample with replacement from integers 1:n, size k
+sample(n,n) #permutation
+sample(letters,7) #letters is default alphabet vector. Gives random 7 letter 'word'
+sample(4,3,replace=TRUE,prob = c(0.1,0.2,0.3,0.4)) 
+#If sample without replacement, the probability of a 
+#yet-to-be-picked element is proportional to it's given probability
+
+#de Montmort
+n <- 100 #100 cards
+r <- replicate(10^4,sum(sample(n,n)==(1:n))) # sample(n,n)==(1:n) is a n-dim vector eg. (0,1,0,...,0). == is TRUE if ith element matches. TRUE is encoded as 1, FALSE as 0. Hence we get the vector (0,0,1,...,0) if there is a match on the third card. 
+#sum adds up the elements of this n-vector. Then replicate does this 10^4 times.
+# r is a vector of length 10^4 containing the number of matched cards from each experiment.
+sum(r>=1)/10^4 # no. of successful experiments divided by the number of simulations.
+
+#Birthday problem
+#To calculate the probability of at least one match in a group of 23 people:
+k <- 23
+1-(prod((365-k+1):365)/365^k) # Counting the complement w/ naive def. approx = 0.5
+
+#Now we /simulate/ the birthday problem.
+s <- 10000
+g <- 23 #prob of at least one match is 0.5
+r <- replicate(s,max(tabulate(sample(365,g,replace=TRUE))))
+sum(r>=2)/s
