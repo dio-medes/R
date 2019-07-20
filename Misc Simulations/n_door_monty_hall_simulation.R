650e16e1b61afbb3727883f2e6aa2326261f9b0d
Oliver Richard Cutbill <olivercutbill@gmail.com>
2019-07-21 09:40:07 +1000

    Now working.

--- Misc Simulations/n_door_monty_hall_simulation.R
+++ Misc Simulations/n_door_monty_hall_simulation.R
@@ -2,11 +2,11 @@
 # If you selected a door, and then Monty opned 999,998 of the door doors, it's pretty 
 # intuitive that one should swap. That is what the below code shows.
 
-# Number of doors:
-k <- 80
+# Number of doors (must be more than 3):
+k <- 4
 
 # Number of Simulations:
-n <- 100
+n <- 10^4
 
 #Repeating code the better way
 do_once <- function()
@@ -15,12 +15,12 @@ do_once <- function()
   choice <- sample(1:k,1)
   true <- sample(1:k,1)
   forbidden <- c(choice,true)
-  canopen <- c(setdiff(1:k,forbidden))
+  canopen <- setdiff(1:k,forbidden)
   
-  opened <- sample(canopen,(k-2),replace = FALSE)
-  swapdoor <- c(setdiff(1:k,c(choice,opened)))
+  opened <- sample(canopen,(k-2), replace = FALSE)
+  swapdoor <- setdiff(1:k,c(choice,opened))
   result <- swapdoor == true
-  
+  result
 }
 success <- sum(replicate(n,do_once())) # TRUE encoded as 1, FALSE as 0.
 prob<- success/n
